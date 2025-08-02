f72ae64f247fa4a24f15e1d295b7b81230c98dee
glopes-a <glopesaraujo6@gmail.com>
2025-08-02 17:47:18 -0300

    codigo resumido

--- rush02.c
+++ rush02.c
@@ -1,49 +1,50 @@
-void    ft_putchar(char c);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glopes-a <glopes-a@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 17:38:01 by glopes-a          #+#    #+#             */
+/*   Updated: 2025/08/02 17:46:48 by glopes-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void    rush(int x, int y)
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
 {
-    int countx;
-    int county;
+	int	countx;
+	int	county;
 
-    county = 0;
-    countx = 0;
-    while (countx < x)
-    {
-        if(countx == 0 || countx == x-1)
-            ft_putchar('A');
-        else 
-            ft_putchar('B'); 
-        countx++;  
-    }
-    ft_putchar('\n');
-    countx=0;    
-    //Esse while percorre verticalmente
-    while (county < y-2)
-    {
-        countx=0;
-        //Esse percorre horizontalmente
-        while(countx < x)
-        {   
-            if (countx==0 || countx==x-1)
-                ft_putchar('B');
-            else
-                ft_putchar(' ');
-            countx++;
-        }
-        ft_putchar('\n');
-        county++;
-    }
-    countx=0;
-    if(y>1)
-    {
-        while(countx < x)
-        {
-            if (countx < 1 || countx == x-1)
-                ft_putchar('C');
-            else
-                ft_putchar('B');
-            countx++;
-        }
-    }
+	countx = 0;
+	county = 0;
+	while (county < y)
+	{
+		countx = 0;
+		while (countx < x)
+		{
+			// linha 1 imprime 'A' na primeira e ultima posicao
+			if (county == 0 && (countx == 0 || countx == x - 1))
+				ft_putchar('A');
+			// linha 1 imprime 'B' quando percorrer pelo meio
+			else if (county == 0)
+				ft_putchar('B');
+			// ultima linha imprime 'C' na primeira e ultima posicao
+			else if (county == y - 1 && (countx == 0 || countx == x - 1))
+				ft_putchar('C');
+			// ultima linha no meio
+			else if (county == y - 1)
+				ft_putchar('B');
+			// linha do meio na primeira e ultima posição
+			else if (countx == 0 || countx == x - 1)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			countx++;
+		}
+		// quebra linha cada vez que o programa percorre uma linha completa
+		ft_putchar('\n');
+		county++;
+	}
 }
-
